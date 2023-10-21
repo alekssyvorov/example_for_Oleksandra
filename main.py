8317d6dfb456bfe827240c0b02838c3059ab8ad8
alekssyvorov <123986551+alekssyvorov@users.noreply.github.com>
2023-10-21 06:19:01 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,45 @@
+# while True:
+#     print('что то выполняется в цикле')
+#     print('это тело цикла')
+
+
+
+# for имяпеременной in рядчисел/итератор:
+#     print('что то выполняется в цикле')
+#     print('это тело цикла')
+
+# lst = [1, 2, 5, 4, 18, 25]
+# s = "Hello"
+# r = range(10)
+# range(start, stop, step)
+
+# lst = [10, 2, 5, 4, 18, 25]
+# print(lst[0])
+# print(lst[1])
+# print(lst[2])
+# a = len(lst)
+# print(a)
+# for i in range(len(lst)):
+#     print(lst[i])
+
+# for element in lst:
+#     print(element)
+
+# if len(password) >= 5:
+#     break
+
+# if len(login) >= 5 and len(login) <= 10:
+#     break
+
+# import random
+
+# from random import randint
+# for i in range(5):
+#     a = randint(100, 500)
+#     print(a)
+
+
+# names = ['Anna', 'Oleg', 'Ivan']
+
+# if i % 2 == 0:
+#     names[i]
